bfdd366f93ba5e08072948c05e803231630b3d07
bmetaj <144210576+butrintmetaj7@users.noreply.github.com>
2025-10-01 15:14:20 +0200

    Add tailwind for styling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import PrimeUI from 'tailwindcss-primeui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [PrimeUI],
+}
+
